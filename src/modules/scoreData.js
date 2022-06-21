fe5d8ba659f5439860722e1c4381ec5eb621a59b
Desor Guishny <desorguishny@gmail.com>
2022-06-21 17:32:54 -0400

    Implement leaderboard structure

---
+++ src/modules/scoreData.js
@@ -0,0 +1,32 @@
+const scoreData = [
+  {
+    name: 'Master',
+    score: '1000'
+  },
+  {
+    name: 'Live',
+    score: '955'
+  },
+  {
+    name: 'Okay',
+    score: '1200'
+  },
+  {
+    name: 'Best',
+    score: '300'
+  },
+  {
+    name: 'Live',
+    score: '789'
+  },
+  {
+    name: 'Young',
+    score: '9999'
+  },
+  {
+    name: 'Gold',
+    score: '994'
+  },
+]
+
+export default scoreData;
