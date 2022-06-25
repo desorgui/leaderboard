62cd1b1b8abdf546f0ac11190cd40f7a8500b19c
Desor Guishny <desorguishny@gmail.com>
2022-06-25 12:28:17 -0400

    Fix ESLint Errors

--- src/modules/createListItem.js
+++ src/modules/createListItem.js
@@ -2,7 +2,7 @@ const listContainer = document.querySelector('#scoreList');
 const createListItem = (user, score, index) => {
   const listItem = document.createElement('li');
   listItem.className = 'item';
-  listItem.id = `item${index}`
+  listItem.id = `item${index}`;
   if (index === 0) {
     listItem.innerHTML += `<span>${user} :</span><span> ${score}</span> <i class="fa-solid fa-crown award"></i>`;
   } else if (index === 1 || index === 2) {
