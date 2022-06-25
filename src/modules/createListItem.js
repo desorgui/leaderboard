fcadf6375e4aef2ff67f0aa927a48fec7d4b98b4
Desor Guishny <desorguishny@gmail.com>
2022-06-25 12:24:26 -0400

    Fix Linters Errors

--- src/modules/createListItem.js
+++ src/modules/createListItem.js
@@ -3,12 +3,12 @@ const createListItem = (user, score, index) => {
   const listItem = document.createElement('li');
   listItem.className = 'item';
   listItem.id = `item${index}`
-  if (index == 0) {
+  if (index === 0) {
     listItem.innerHTML += `<span>${user} :</span><span> ${score}</span> <i class="fa-solid fa-crown award"></i>`;
-  } else if (index == 1 || index == 2) {
+  } else if (index === 1 || index === 2) {
     listItem.innerHTML += `<span>${user} :</span><span> ${score}</span> <i class="fa-solid fa-medal award"></i>`;
   } else {
-      listItem.innerHTML += `<span>${user} :</span><span> ${score}</span> <i class="fa-solid fa-star award"></i>`;
+    listItem.innerHTML += `<span>${user} :</span><span> ${score}</span> <i class="fa-solid fa-star award"></i>`;
   }
   listContainer.appendChild(listItem);
 };
